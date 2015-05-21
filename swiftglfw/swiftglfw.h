4789206b659c2770a765c030cfd12bbe10ec3b88
David Green <shinohara@gmail.com>
2015-05-21 02:05:01 -0400

    Created project.

---
+++ swiftglfw/swiftglfw.h
@@ -0,0 +1,19 @@
+//
+//  swiftglfw.h
+//  swiftglfw
+//
+//  Created by David Green on 5/21/15.
+//  Copyright (c) 2015 David Green. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftglfw.
+FOUNDATION_EXPORT double swiftglfwVersionNumber;
+
+//! Project version string for swiftglfw.
+FOUNDATION_EXPORT const unsigned char swiftglfwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftglfw/PublicHeader.h>
+
+
